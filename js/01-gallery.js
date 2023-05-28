66382e7298e4d27442158332582315a784a6689f
ng-christina <118376146+ng-christina@users.noreply.github.com>
2023-05-28 19:49:31 +0300

    update 1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,55 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const fotoCard = galleryFoto(galleryItems) 
+ gallery.insertAdjacentHTML('beforeend', fotoCard);
+
+ function galleryFoto(galleryItems) {
+    return galleryItems
+    .map(({preview, original, description }) =>{
+    return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+    })
+    .join('')
+ };
+ gallery.addEventListener("click", galleryClick);
+ function galleryClick(evt) {
+  evt.preventDefault();
+if (evt.target.nodeName !== 'IMG') {
+    return;
+}
+  const instance = basicLightbox.create(
+    `
+  <img src="${evt.target.dataset.source}" alt="${evt.target.alt}">
+  `,
+{
+  onShow: (instance) => { 
+  window.addEventListener("keydown", escKeyPess);
+},
+
+onClose: (instance) => { 
+  window.removeEventListener("keydown", escKeyPess);
+},
+}
+);
+instance.show();  
+
+function escKeyPess(evt) {
+  if (evt.code === 'Escape') {
+    instance.close() 
+  }
+}
+ }
+
+
