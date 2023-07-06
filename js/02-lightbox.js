959639174c776437206b0bf0773a54babed7d70e
ng-christina <118376146+ng-christina@users.noreply.github.com>
2023-07-06 21:48:31 +0300

    гз

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,33 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
+const gallery = document.querySelector(".gallery");
+const fotoCard = galleryFoto(galleryItems);
+gallery.insertAdjacentHTML("beforeend", fotoCard);
 
-const gallery = document.querySelector('.gallery');
-const fotoCard = galleryFoto(galleryItems) 
- gallery.insertAdjacentHTML('beforeend', fotoCard);
-
- function galleryFoto(galleryItems) {
-    return galleryItems
-    .map(({preview, original, description }) =>{
-        return`
+function galleryFoto(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
         <li class="gallery__item">
             <a class="gallery__link" href="${original}">
                <img class="gallery__image" src="${preview}" alt="${description}" />
             </a>
-        </li>`
-        })
-    .join('');
+        </li>`;
+    })
+    .join("");
+}
+const leigthBox = new SimpleLightbox(".gallery__link", {
+  navText: ["<", ">"],
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
- }
- const leigthBox = new SimpleLightbox(".gallery__link", { 
-    navText : ['<','>'],
-    captionsData: 'alt',
-    captionDelay : 250
-})
-
-  gallery.addEventListener("click", galleryClick);
- function galleryClick(evt) {
+gallery.addEventListener("click", galleryClick);
+function galleryClick(evt) {
   evt.preventDefault();
- }
+}
